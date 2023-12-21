c351f5ba3792542c918ef4521284a3ae4687c52a
Emil Sauer Lynge <ely@muninn.ai>
2023-12-21 13:04:48 +0100

    make libs public

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ pub mod netmap_user;
 mod netmap_util;
 
 #[cfg(feature = "netmap_with_libs")]
-mod netmap_with_libs;
+pub mod netmap_with_libs;
 
 #[cfg(feature = "libnetmap")]
-mod libnetmap;
+pub mod libnetmap;
